0311054add1eede21461688cb0880cea0d76ae76
HCgittt <3538371984@qq.com>
2022-06-10 17:35:27 +0800

    准备好HTML元素和基本样式

---
+++ src2/main.js
@@ -0,0 +1,7 @@
+let player = document.querySelector('player');
+let left = document.querySelector("left");
+let right = document.querySelector('right');
+
+left.addEventListener('click', (e)=>{
+    console.log(e.target)
+})

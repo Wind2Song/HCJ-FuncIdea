edf7de7776db89013d290fa22df180efa9fe807d
HCgittt <3538371984@qq.com>
2022-06-11 09:34:27 +0800

    获取到player宽度

--- src2/main.js
+++ src2/main.js
@@ -1,7 +1,17 @@
-let player = document.querySelector('player');
-let left = document.querySelector("left");
+let player = document.querySelector('.player');
+let left = document.querySelector("#left");
 let right = document.querySelector('right');
-
+let container = document.querySelector('#container');
+console.log(player)
 left.addEventListener('click', (e)=>{
-    console.log(e.target)
+    const styleRight = getComputedStyle(player).right
+    const width = getComputedStyle(player).width
+    console.log(styleRight)
+    console.log(width)
+    
 })
+// console.log('成功引入js')
+// console.log(container)
+// container.addEventListener('click', (e)=>{
+//     console.log('111')
+// })

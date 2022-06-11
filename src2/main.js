0253911fa736d32241d4840b18d52f91c0abdd91
HCgittt <3538371984@qq.com>
2022-06-11 10:41:00 +0800

    左右按钮样式添加完毕

--- src2/main.js
+++ src2/main.js
@@ -1,17 +1,39 @@
-let player = document.querySelector('.player');
-let left = document.querySelector("#left");
-let right = document.querySelector('right');
-let container = document.querySelector('#container');
-console.log(player)
-left.addEventListener('click', (e)=>{
-    const styleRight = getComputedStyle(player).right
-    const width = getComputedStyle(player).width
-    console.log(styleRight)
-    console.log(width)
-    
-})
+// let player = document.querySelector('.player');
+// let left = document.querySelector("#left");
+// let right = document.querySelector('right');
+// console.log(player)
+// left.addEventListener('click', (e)=>{
+//     const styleRight = getComputedStyle(player).right
+//     const width = getComputedStyle(player).width.slice(0,-2)
+//     console.log(styleRight)
+//     // console.log(width)
+//     player.style.right = width + 'px'
+// })
 // console.log('成功引入js')
 // console.log(container)
 // container.addEventListener('click', (e)=>{
 //     console.log('111')
 // })
+
+let index = 0;
+
+function refresh(){
+    // 获取轮播包裹元素.width
+    let container = document.querySelector('#container');
+
+    let {width} = getComputedStyle(container);
+    width = Number(width.slice(0, -2));
+
+    container.querySelector('.player').style.right = index * width;
+}
+
+function left(){
+    index++;
+    refresh()
+}
+function right(){
+    index--;
+    refresh();
+}
+
+refresh();

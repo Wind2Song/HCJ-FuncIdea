eecb87a4ac493621b62e3ebd66adc1916cef4e8c
HCgittt <3538371984@qq.com>
2022-06-26 15:58:36 +0800

    五li好评填充

--- FiveScore/main.js
+++ FiveScore/main.js
@@ -2,18 +2,32 @@ console.log('hi')
 
 let arr = document.querySelectorAll(".container ul li")
 // console.log(arr[0])
+// for(let i=0;i<arr.length;i++){
+//     arr[i].click = (e)=>{
+//         console.log(1)
+//     }
+// }
 
 let ul = document.querySelector(".container ul")
-// console.log(ul)
-ul.addEventListener('click',(e)=>{
+ul.onmouseover = (e)=>{
     // console.log(e.target)
     for(let i=0;i<arr.length;i++){
         if(arr[i].textContent <= e.target.textContent){
-            // let background = getComputedStyle(arr[i]).background
-            // console.log(getComputedStyle(arr[i]).backgroundColor)
-            // arr[i].style.background = background.replace('rgba(0, 0, 0, 0)','rgba(255, 255, 255, 0)')
+            for(let l = 0;l<=i;l++){
+                arr[i].style.backgroundColor = 'red';
+            }
+        }else{
+            for(let l=i+1;l<=arr.length;l++){
+                arr[i].style.backgroundColor = '';
+            }
+        }
+    }
+}
+ul.addEventListener('onmouseover',(e)=>{
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].textContent <= e.target.textContent){
             arr[i].style.backgroundColor = 'red';
-            
+            // localStorage.backgroundColor = 'red'
         }
     }
 })

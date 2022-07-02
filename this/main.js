9837f81061f239cc62ff458835fd663937d1f08e
HCgittt <3538371984@qq.com>
2022-07-02 11:55:33 +0800

    外部访问函数内部的变量：构造函数变普通

--- this/main.js
+++ this/main.js
@@ -24,24 +24,24 @@
 // j()
 
 
-var x = 3;
-var y = 4;
-var obj = {
-    x:1,
-    y:6,
-    getX:function(){
-        var x = 5;
-        return function(){
-            console.log('---')
-            console.log(this)
-            return this.x;
-        }()
-    },
-    getY:function(){
-        var y=7;
-        return this.y
-    }
-}
-console.log(window.xxx)
-console.log(obj.getX())
-console.log(obj.getY())
+// var x = 3;
+// var y = 4;
+// var obj = {
+//     x:1,
+//     y:6,
+//     getX:function(){
+//         var x = 5;
+//         return function(){
+//             console.log('---')
+//             console.log(this)
+//             return this.x;
+//         }()
+//     },
+//     getY:function(){
+//         var y=7;
+//         return this.y
+//     }
+// }
+// console.log(window.xxx)
+// console.log(obj.getX())
+// console.log(obj.getY())

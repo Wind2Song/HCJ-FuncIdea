fff00548f5a26122e145690a6bc11099c4793d56
HCgittt <3538371984@qq.com>
2022-07-04 13:26:32 +0800

    new是个操作符，配合构造函数使用

--- this/main.js
+++ this/main.js
@@ -61,11 +61,11 @@ function biBao(){
 
 var arr = [1,2,3,4,5,6,7,8,9]
 
-for(var i=0;i<arr.length;i++){
-    setTimeout(()=>{
-        console.log(i)
-    },i*100,i)
-}
+// for(var i=0;i<arr.length;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },i*100,i)
+// }
 
 // 牛刀小试
 function foo(){
@@ -81,4 +81,6 @@ function foo(){
 }
 var obj = foo()
 var age = obj.getNum()
-console.log(age)
+// console.log(age)
+
+
